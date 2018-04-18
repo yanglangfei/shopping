a7d5a0aba10fdffdcbc3fb41237d572a6b0efa1e
Yanglangfei <yanglangfei@jucaipen.com>
2018-04-18 17:05:51 +0800

    web

---
+++ h5-web/src/main/java/com/yf/web/H5WebApplication.java
@@ -0,0 +1,13 @@
+package com.yf.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H5WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H5WebApplication.class,args);
+    }
+
+}
